cf847605191a2fd4e294473c518a265847d0e9f8
sumit-krk <kushakk007@gmail.com>
2023-01-31 02:19:41 +0530

    now connecting server to databse in index file

--- TODO/todo-backend/db.js
+++ TODO/todo-backend/db.js
@@ -1,29 +1,33 @@
 const mongoose=require("mongoose");
-
-const main=async ()=>{
-    try{
-        const connection=await mongoose.connect("mongodb://127.0.0.1:27017/todo")
-        console.log("connected to mongo")
-        // await TodoModel.insertMany([{text:'React Task',is_done:false},{text:'View Task',is_done:false}])
-        const task=new TodoModel({
-            text:'Node Js',
-            is_done:false
-        })
-        await task.save()
-    }catch(err){
-        console.log("something went wrong")
-        console.log(err)
-    }
+const connection=await mongoose.connect("mongodb://127.0.0.1:27017/todo")
+module.exports={
+    connection
 }
 
-const todoSchema=mongoose.Schema({
-    text:{type:String,required:true},
-    is_done:{type:Boolean,required:true}
-},{
-    versionKey:false
-})
+// const main=async ()=>{
+//     try{
+//         const connection=await mongoose.connect("mongodb://127.0.0.1:27017/todo")
+//         console.log("connected to mongo")
+//         // await TodoModel.insertMany([{text:'React Task',is_done:false},{text:'View Task',is_done:false}])
+//         const task=new TodoModel({
+//             text:'Node Js',
+//             is_done:false
+//         })
+//         await task.save()
+//     }catch(err){
+//         console.log("something went wrong")
+//         console.log(err)
+//     }
+// }
 
-const TodoModel=mongoose.model("task",todoSchema)
+// const todoSchema=mongoose.Schema({
+//     text:{type:String,required:true},
+//     is_done:{type:Boolean,required:true}
+// },{
+//     versionKey:false
+// })
 
-main();
+// const TodoModel=mongoose.model("task",todoSchema)
+
+// main();
 
--- TODO/todo-backend/index.js
+++ TODO/todo-backend/index.js
@@ -1,6 +1,15 @@
 const express=require("express");
 const app=express();
+import {connection} from './db';
 
-app.listen('3400',()=>{
+app.listen('3400',async()=>{
+   try{
+    await connection
+    console.log("data base connected")
+   }
+   catch(err){
+        console.log("error while connecting");
+        console.log(err);
+   }
     console.log("app listen on port 3400");
 })
