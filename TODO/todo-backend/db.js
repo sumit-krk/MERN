27db562e87f622926232b5f9d1ffa6fd2ed098c4
sumit-krk <kushakk007@gmail.com>
2023-01-31 00:26:40 +0530

    added single document

--- TODO/todo-backend/db.js
+++ TODO/todo-backend/db.js
@@ -2,13 +2,28 @@ const mongoose=require("mongoose");
 
 const main=async ()=>{
     try{
-        const connection=await mongoose.connect("mongodb://127.0.0.1:27017/")
+        const connection=await mongoose.connect("mongodb://127.0.0.1:27017/todo")
         console.log("connected to mongo")
+        // await TodoModel.insertMany([{text:'React Task',is_done:false},{text:'View Task',is_done:false}])
+        const task=new TodoModel({
+            text:'Node Js',
+            is_done:false
+        })
+        await task.save()
     }catch(err){
         console.log("something went wrong")
         console.log(err)
     }
 }
 
+const todoSchema=mongoose.Schema({
+    text:String,
+    is_done:Boolean
+},{
+    versionKey:false
+})
+
+const TodoModel=mongoose.model("task",todoSchema)
+
 main();
-console.log("hum first")
+
